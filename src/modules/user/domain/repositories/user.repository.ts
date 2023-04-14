4ebf6676977bca045a49d013bb2d6c216a992de1
Jose Junior <juniorferreira1919@hotmail.com>
2023-04-13 21:12:31 -0300

    refactor: restart the project

--- src/modules/user/domain/repositories/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../entities/user.entity';
-
-export abstract class UserRepository {
-  abstract create(user: User): Promise<void>;
-  abstract findByEmail(email: string): Promise<User>;
-}
