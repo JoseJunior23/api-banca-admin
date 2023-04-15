b2467cb7a72d65e64e682fbbefed1cfad585c50e
Jose Junior <juniorferreira1919@hotmail.com>
2023-04-15 14:14:17 -0300

    feat: add migration create user/ mappers user

---
+++ src/modules/user/infra/database/mappers/prisma-user-mapper.ts
@@ -0,0 +1,23 @@
+import { User } from '@modules/user/domain/entities/user';
+import { User as RawUser } from '@prisma/client';
+export class PrismaUserMapper {
+  static toPrisma(user: User) {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    };
+  }
+
+  static toDomain(raw: RawUser): User {
+    return new User({
+      id: raw.id,
+      name: raw.name,
+      email: raw.email,
+      password: raw.password,
+      created_at: raw.created_at,
+      updated_at: raw.updated_at,
+    });
+  }
+}
